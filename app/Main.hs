f0c447ec93936db9fa55f007f9424c4009bbd675
Ludvig Sundström <lud.sund@gmail.com>
2019-10-07 20:53:14 +0200

    MVP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Util
+import Data.List.Split
+
+nCards :: Int
+nCards = 5
+
+questionsFile :: FilePath
+questionsFile = "/home/lsund/Documents/work/notes/kafka.md"
+
+fileContentToCards :: String -> [(String, String)]
+fileContentToCards = map (\[x, y] -> (x, y)) . take nCards . chunksOf 2 . filter (not . null) . lines
+
+userQuery :: (String, String) -> IO String
+userQuery (question, answer) = do
+  putStrLn $ question ++ " [Enter]"
+  getLine
+  putStrLn answer
+  putStrLn "Correct?"
+  getLine
+userQuery _ = error "Malformed data"
 
 main :: IO ()
-main = someFunc
+main = do
+  x <- readFile questionsFile >>= \cont ->
+    mapM userQuery $ fileContentToCards cont
+  print x
