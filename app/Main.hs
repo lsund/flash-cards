46ae276cd4220138b494a98eefb08f7920f5dc5d
Ludvig Sundström <lud.sund@gmail.com>
2019-10-07 20:59:00 +0200

    Minor refactor

--- app/Main.hs
+++ app/Main.hs
@@ -2,26 +2,32 @@ module Main where
 
 import Data.List.Split
 
+-- (Question, Answer)
+type Card = (String, String)
+
 nCards :: Int
 nCards = 5
 
 questionsFile :: FilePath
 questionsFile = "/home/lsund/Documents/work/notes/kafka.md"
 
-fileContentToCards :: String -> [(String, String)]
-fileContentToCards = map (\[x, y] -> (x, y)) . take nCards . chunksOf 2 . filter (not . null) . lines
+fileContentToCards :: String -> [Card]
+fileContentToCards =
+  map tuplize . take nCards . chunksOf 2 . filter (not . null) . lines
+  where
+    tuplize [x, y] = (x, y)
+    tuplize _ = error "Malformed data"
 
-userQuery :: (String, String) -> IO String
+userQuery :: Card -> IO String
 userQuery (question, answer) = do
-  putStrLn $ question ++ " [Enter]"
-  getLine
+  putStr $ question ++ " [Enter]"
+  _ <- getLine
   putStrLn answer
-  putStrLn "Correct?"
+  putStrLn "Y/n"
   getLine
-userQuery _ = error "Malformed data"
 
 main :: IO ()
 main = do
-  x <- readFile questionsFile >>= \cont ->
-    mapM userQuery $ fileContentToCards cont
+  x <-
+    readFile questionsFile >>= \cont -> mapM userQuery $ fileContentToCards cont
   print x
